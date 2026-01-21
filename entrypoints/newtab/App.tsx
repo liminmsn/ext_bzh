2aa2e5c6552dab0e2d9eaf57918f0d752f8a167f
杨再涛 <1772081518@qq.com>
2026-01-22 01:16:27 +0800

    初始化好启动页和popup页

--- entrypoints/newtab/App.tsx
+++ entrypoints/newtab/App.tsx
@@ -1,16 +1,7 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-
 export default function App() {
   return (
-    <div style={{ padding: 32 }}>
-      <h1>🚀 WXT React 新标签页</h1>
-      <p>这是一个 React 项目的启动页</p>
+    <div className="h-full bg-black">
+      21
     </div>
   )
-}
-
-const container = document.getElementById('root')
-if (container) {
-  createRoot(container).render(<App />)
 }
