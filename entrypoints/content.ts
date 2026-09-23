bf0a695778c28c8316d7662c93b031d6d7b3435f
杨再涛 <1772081518@qq.com>
2026-09-24 02:18:35 +0800

    Refactor code structure for improved readability and maintainability

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,6 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
+  matches: ['https://forum.cocos.org/*'],
   main() {
-    console.log('Hello content.');
+    console.log('Hello content. cocos');
   },
 });
